d9fc09a5b6615f80f18f1954590cd1214aaf1227
elnara002 <6fdtxsg@code.edu.az>
2024-03-07 01:06:02 +0400

    loops-hometask

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+// var phoneBrands = ["Acer", "Apple", "Sony", "Samsung"];
+// var bookList = [
+//   "Calculus Early Transcendentals",
+//   "Ravens",
+//   "The Self Illusion",
+//   "Harry Potter"
+// ];
+// var movieList = [
+//   "Titanic",
+//   "Interstaller",
+//   "The Shawshank Redemption",
+//   "The Martian"
+// ];
+// for (var i = 0; i < phoneBrands.length; i++) {
+//   console.log(phoneBrands[i]);
+// }
+// for (var k = 0; k < bookList.length; k++) {
+//   console.log(bookList[k]);
+// }
+// for (var j = 0; j < bookList.length; j++) {
+//   console.log(movieList[j]);
+// }
+
+var vegetablesName = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+for (var i = 0; i < vegetablesName.length; i++) {
+// console.log("I love " +" "+ vegetablesName[i])
+  console.log(`I like ${vegetablesName[i]}`)
+}
+     
+      
+
